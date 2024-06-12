caa816597711298467fcd1bda7605d7a043ec0a4
mingsheng <ming.chai@ucdconnect.ie>
2024-06-12 19:44:45 +0100

    update

--- catppuccin/script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-text=$(<$1)
-echo "$text" | sed -E -e '/(\<th\>|rgb|hsl|summary)/d' -e 's/[[:space:]]*<[^>]*>[[:space:]]*//g' -e '/^[[:space:]]*$/d' | awk 'NR%2{printf $0"="; next}{print $0}' | awk 'BEGIN {FS="#";OFS="#ff";print "#!/usr/bin/env bash"} {$1=$1;print}' > $1.sh
--- catppuccin/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source mocha.sh
-echo "$Pink"
