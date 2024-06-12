0cea7736250cd0196f9945c66a5d31807d10b69d
mingsheng <ming.chai@ucdconnect.ie>
2024-06-12 19:54:46 +0100

    added dracula

---
+++ dracula/color.sh
@@ -0,0 +1,12 @@
+Background=#ff282a36
+Current_Line=#ff44475a
+Selection=#ff44475a
+Foreground=#fff8f8f2
+Comment=#ff6272a4
+Cyan=#ff8be9fd
+Green=#ff50fa7b
+Orange=#ffffb86c
+Pink=#ffff79c6
+Purple=#ffbd93f9
+Red=#ffff5555
+Yellow=#fff1fa8c
